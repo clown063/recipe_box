865fd70bc6bd61c6e7fe9bd199568ff6d8c451ae
Naoya <naoyaclown6363@icloud.com>
2023-02-14 13:17:44 +1000

    Feb 14 13 17

--- php/components/upper.php
+++ php/components/upper.php
@@ -4,7 +4,6 @@
         <!-- Meta from Template -->
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">	
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-        <meta http-equiv="Cache-control" content="no-cache">
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="mobile-web-app-capable" content="yes">
         <!-- Link from Template -->
